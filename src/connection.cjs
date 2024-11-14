2e251b45ac421ac2d157e0d304899eb4452b3c99
Vincent Br <44780692+Vincent-Br@users.noreply.github.com>
2024-11-14 12:07:19 +0100

    Removed OTTD_USERNAME variable

--- src/connection.cjs
+++ src/connection.cjs
@@ -1,57 +1,56 @@
 const ottd = require("node-openttd-admin");
 
 const ottdConnection = new ottd.connection();
-ottdConnection.connect(process.env.OTTD_IP, process.env.OTTD_PORT);
+ottdConnection.connect(process.env.OTTD_HOST, process.env.OTTD_PORT);
 
 ottdConnection.on("connect", () =>
-    ottdConnection.authenticate(
-        process.env.OTTD_USERNAME,
-        process.env.OTTD_PASSWORD
-    )
+  ottdConnection.authenticate("OpenTTD-Exporter", process.env.OTTD_PASSWORD)
 );
 ottdConnection.on("error", (data) => console.log(data));
 
 ottdConnection.on("welcome", (data) => {
-    sendChatMessage("+++-----------------------------------------------+++");
-    sendChatMessage("OpenTTD-exporter has been started! Game data is being logged!");
-    sendChatMessage("+++-----------------------------------------------+++");
-    requestUpdateFrequency();
+  sendChatMessage("+++-----------------------------------------------+++");
+  sendChatMessage(
+    "OpenTTD-exporter has been started! Game data is being logged!"
+  );
+  sendChatMessage("+++-----------------------------------------------+++");
+  requestUpdateFrequency();
 });
 
 function sendChatMessage(message) {
-    ottdConnection.send_chat(
-        ottd.enums.Actions.SERVER_MESSAGE,
-        ottd.enums.DestTypes.BROADCAST,
-        "",
-        message
-    );
+  ottdConnection.send_chat(
+    ottd.enums.Actions.SERVER_MESSAGE,
+    ottd.enums.DestTypes.BROADCAST,
+    "",
+    message
+  );
 }
 
 function requestUpdateFrequency() {
-    ottdConnection.send_update_frequency(
-        ottd.enums.UpdateTypes.DATE,
-        ottd.enums.UpdateFrequencies.WEEKLY
-    );
+  ottdConnection.send_update_frequency(
+    ottd.enums.UpdateTypes.DATE,
+    ottd.enums.UpdateFrequencies.WEEKLY
+  );
 
-    ottdConnection.send_update_frequency(
-        ottd.enums.UpdateTypes.COMPANY_INFO,
-        ottd.enums.UpdateFrequencies.AUTOMATIC
-    );
+  ottdConnection.send_update_frequency(
+    ottd.enums.UpdateTypes.COMPANY_INFO,
+    ottd.enums.UpdateFrequencies.AUTOMATIC
+  );
 
-    ottdConnection.send_update_frequency(
-        ottd.enums.UpdateTypes.COMPANY_ECONOMY,
-        ottd.enums.UpdateFrequencies.WEEKLY
-    );
+  ottdConnection.send_update_frequency(
+    ottd.enums.UpdateTypes.COMPANY_ECONOMY,
+    ottd.enums.UpdateFrequencies.WEEKLY
+  );
 
-    ottdConnection.send_update_frequency(
-        ottd.enums.UpdateTypes.COMPANY_STATS,
-        ottd.enums.UpdateFrequencies.WEEKLY
-    );
+  ottdConnection.send_update_frequency(
+    ottd.enums.UpdateTypes.COMPANY_STATS,
+    ottd.enums.UpdateFrequencies.WEEKLY
+  );
 
-    ottdConnection.send_update_frequency(
-        ottd.enums.UpdateTypes.CLIENT_INFO,
-        ottd.enums.UpdateFrequencies.AUTOMATIC
-    );
+  ottdConnection.send_update_frequency(
+    ottd.enums.UpdateTypes.CLIENT_INFO,
+    ottd.enums.UpdateFrequencies.AUTOMATIC
+  );
 }
 
 module.exports = { ottdConnection };
